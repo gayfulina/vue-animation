83f7ea4e5fd5ec2dd36ea739ca1c46e2365bc309
gayfulina <irinmdr@gmail.com>
2021-12-18 12:05:00 -0800

    transition

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
     <div class='block' :class='{animate: animatedBlock}'></div>
     <button @click='animateBlock'>Animate</button>
   </div>
+
+  <div class='container'>
+    <transition>
+    <p v-if='paraIsVisible'>This is only sometimes visible...</p>
+    </transition>
+
+    <button @click='toggleParagraph'>Toggle Paragraph</button>
+  </div>
+
   <base-modal @close='hideDialog' v-if='dialogIsVisible'>
     <p>This is a test dialog!</p>
     <button @click='hideDialog'>Close it!</button>
@@ -17,7 +26,8 @@ export default {
   data() {
     return {
       animatedBlock: false,
-      dialogIsVisible: false
+      dialogIsVisible: false,
+      paraIsVisible: false,
     };
   },
   methods: {
@@ -29,6 +39,9 @@ export default {
     },
     hideDialog() {
       this.dialogIsVisible = false;
+    },
+    toggleParagraph() {
+      this.paraIsVisible = ! this.paraIsVisible;
     }
   }
 };
